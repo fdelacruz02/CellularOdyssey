33dc377aa981be660610a5a7a2593d9d21dde8f5
fdelacruz02 <fadelacruz@students.pccc.edu>
2024-11-13 06:40:02 -0500

    fixed some files

--- website/connection.php
+++ website/connection.php
@@ -1,9 +1,4 @@
 <?php 
-    $host = '127.0.0.1';
-    $db   = 'bwqkeimy_TrialDatabase';
-    $user = 'bwqkeimy_GGDatabaseAdmin';
-    $pass = '}h}WwXSm[u2n';
-
     $dsn = "mysql:host=$host;dbname=$db;";
     try {
         $pdo = new PDO($dsn, $user, $pass);
